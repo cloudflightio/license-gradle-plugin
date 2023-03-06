d5e9043988e99555b8b554dfa81640e1720ed52d
Klaus Lehner <172195+klu2@users.noreply.github.com>
2023-03-06 11:05:12 +0100

    update plugin for plugin-publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-gradle-plugin")
     id("maven-publish")
-    id("com.gradle.plugin-publish") version "0.18.0"
+    id("com.gradle.plugin-publish") version "1.1.0"
     alias(libs.plugins.kotlin.serialization)
 }
 
@@ -41,19 +41,16 @@ tasks.compileKotlin.configure {
     }
 }
 
-pluginBundle {
-    website = "https://github.com/cloudflightio/license-gradle-plugin"
-    vcsUrl = "https://github.com/cloudflightio/license-gradle-plugin.git"
-    tags = listOf("license", "dependencies", "report")
-}
-
 gradlePlugin {
+    website.set("https://github.com/cloudflightio/license-gradle-plugin")
+    vcsUrl.set("https://github.com/cloudflightio/license-gradle-plugin.git")
     plugins {
         create("license-gradle-plugin") {
             id = "io.cloudflight.license-gradle-plugin"
             displayName = "License Gradle Plugin"
             description = "Collects and prints all dependencies along with their licenses including SPDX support"
             implementationClass = "io.cloudflight.license.gradle.LicensePlugin"
+            tags.set(listOf("license", "dependencies", "report"))
         }
     }
 }
