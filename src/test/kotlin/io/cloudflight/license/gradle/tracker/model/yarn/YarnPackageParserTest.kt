2d377f300a8a288d1c8f20dcb12568f032859b09
Kangrui Ye <kangrui.ye@cloudflight.io>
2023-10-05 09:19:21 +0200

    #29 check for npm prefix in yarn parser

--- src/test/kotlin/io/cloudflight/license/gradle/tracker/model/yarn/YarnPackageParserTest.kt
+++ src/test/kotlin/io/cloudflight/license/gradle/tracker/model/yarn/YarnPackageParserTest.kt
@@ -14,12 +14,23 @@ class YarnPackageParserTest {
         )
 
         assertThat(module.compile)
-            .anyMatch { it.artifact == "@angular:animations:14.2.10" }
-            .anyMatch { it.artifact == "@npm:tslib:2.4.1" }
+            .anyMatch { it.artifact == "@angular:animations:16.2.8" }
+            .anyMatch { it.artifact == "@npm:tslib:2.6.2" }
             .noneMatch { it.artifact == "@angular-devkit:build-angular" }
 
         assertThat(module.development)
-            .anyMatch { it.artifact.contains("@angular-devkit:build-angular:14.2.9") }
+            .anyMatch { it.artifact.contains("@angular-devkit:build-angular:16.2.5") }
+    }
 
+    @Test
+    fun findNpmPackages() {
+        val licenses = YarnPackageParser.findNpmPackages(
+            File("src/test/resources/yarn/package.json"),
+            File("src/test/resources/yarn/yarn.lock")
+        )
+
+        // parsing licenses is not implemented yet
+        // this test still provides value by testing the parser
+        assertThat(licenses).isEmpty();
     }
 }
